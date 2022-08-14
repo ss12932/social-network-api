2236deb08fdc480976d54f2b17c1a6db11be0e15
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-14 11:53:54 +0100

    route folders + filenames

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const connectToDatabase = require('./config/connection');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 const init = async () => {
   try {
