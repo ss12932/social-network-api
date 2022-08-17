7142367a3e1c849bb250b797d452a513a7edc18b
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-17 18:24:13 +0100

    added fakerjs pkg to add dummy data

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,14 +1,17 @@
 const connectToDatabase = require('../config/connection');
 const { User, Thought, Reaction } = require('../models');
 const seedUsers = require('./users');
+const seedThoughts = require('./thoughts');
 
 const init = async () => {
   try {
     await connectToDatabase();
 
     await User.deleteMany({});
+    await Thought.deleteMany({});
 
     await seedUsers();
+    await seedThoughts();
   } catch (err) {
     console.log(`Failed to seed in Mongo Database || ${err.message}`);
   }
---
+++ src/seeds/thoughts.js
@@ -0,0 +1,7 @@
+const { Thought, User } = require('../models');
+
+const createThoughts = async () => {};
+
+const seedThoughts = async () => {};
+kq;
+module.exports = seedThoughts;
