ea4ed11b36df54e15186c5dab3116f1181bd42a1
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-17 16:50:56 +0100

    finished user seed data + dummy data

--- src/seeds/users.js
+++ src/seeds/users.js
@@ -1,1 +1,23 @@
 const { User } = require('../models');
+
+const users = [
+  { username: 'sinh1234', email: 'sinh1234@email.com' },
+  { username: 'hamed5678', email: 'hamed5678@email.com' },
+  { username: 'sina9999', email: 'sina9999@email.com' },
+  { username: 'fred1234', email: 'fred1234@email.com' },
+  { username: 'johndoe', email: 'johndoe@hotmail.com' },
+];
+
+const seedUsers = async () => {
+  try {
+    const userPromises = users.map((user) => User.create(user));
+
+    await Promise.all(userPromises);
+
+    console.log('Successfully seeded users data.');
+  } catch (err) {
+    console.log(`Failed to seed users data || ${err.message}`);
+  }
+};
+
+module.exports = seedUsers;
