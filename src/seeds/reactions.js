896ce3b080283a890026f201dc0dc8619788b4f3
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-22 16:53:18 +0100

    finished reaction seed file

--- src/seeds/reactions.js
+++ src/seeds/reactions.js
@@ -2,27 +2,41 @@ const { Thought, User, Reaction } = require('../models');
 const { faker } = require('@faker-js/faker');
 
 const generateDummyReactions = async () => {
-  const usersFromDB = await User.find({});
+  //retrieve all thoughts from database
   const thoughtsFromDB = await Thought.find({});
 
+  // loop over all the thoughts from database, for each thought we only have the author's username, use this to find in User collection to obtain extra information like the friends array.
   for (let i = 0; i < thoughtsFromDB.length; i++) {
+    const thoughtAuthor = await User.find({
+      username: thoughtsFromDB[i].username,
+    });
+    // we are creating 3 reactions for each thought, using the friends array from the author we will randomly select a friend id to generate a reaction.
     for (let j = 0; j < 3; j++) {
       const reactionBody = faker.lorem.lines(3);
       const createdAt = faker.date.recent();
-      const username =
-        usersFromDB[Math.floor(Math.random() * usersFromDB.length)].username;
+      const username_id =
+        thoughtAuthor[0].friends[
+          Math.floor(Math.random() * thoughtAuthor[0].friends.length)
+        ]._id;
 
+      // using the friend id obtained above, we will search in the user collection for his document. We need to obtain extra information eg. his username since the author's friends array only contains ids.
+      const findUsernameById = await User.findById(username_id);
+      console.log(findUsernameById);
+
+      // with the friend's related document found above, we will create his reaction using his usename. this will satisfy every single field to create a reaction.
       const generateReaction = await Reaction.create({
         reactionBody,
         createdAt,
-        username,
+        username: findUsernameById.username,
       });
 
+      // we need to update the thoughts' reactions array as well by pushing the reaction id.
       await Thought.findByIdAndUpdate(thoughtsFromDB[i]._id, {
         $push: {
           reactions: generateReaction._id,
         },
       });
+      //repeat this process 3 times to create 3 reactions for each thought.
     }
   }
 };
--- src/seeds/thoughts.js
+++ src/seeds/thoughts.js
@@ -2,18 +2,22 @@ const { Thought, User } = require('../models');
 const { faker } = require('@faker-js/faker');
 
 const generateDummyThoughts = async () => {
+  //obtain all the users from database
   const usersFromDB = await User.find({});
 
+  //loop over all the users
   for (let i = 0; i < usersFromDB.length; i++) {
     const thoughtText = faker.lorem.lines(3);
     const createdAt = faker.date.past();
 
+    //we will generate jsut a thought for each user.
     const generateThought = await Thought.create({
       username: usersFromDB[i].username,
       thoughtText,
       createdAt,
     });
 
+    //we also need to update this user's thoughts array with their thought id.
     await User.findByIdAndUpdate(usersFromDB[i]._id, {
       $push: {
         thoughts: generateThought._id,
