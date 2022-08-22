6a1d507d9c9d0c8b4031caaf709606a4301d9209
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-22 23:03:31 +0100

    reaction controllers done

--- src/controllers/api/reactions.js
+++ src/controllers/api/reactions.js
@@ -1,8 +1,43 @@
-const createNewReactionForThought = (req, res) => {
-  res.send('createNewReactionForThought');
+const createNewReactionForThought = async (req, res) => {
+  try {
+    const { thoughtId } = req.params;
+
+    const data = await Thought.findByIdAndUpdate(
+      thoughtId,
+      {
+        $push: { reactions: { ...req.body } },
+      },
+      { returnOriginal: false }
+    );
+
+    return res.json({ success: true, data });
+  } catch (err) {
+    console.log(`Failed to create new reaction for thought || ${err.message}`);
+    return res.status(500).json({
+      success: false,
+      error: 'Failed to create new reaction for thought ',
+    });
+  }
 };
-const deleteReactionForThought = (req, res) => {
-  res.send('deleteReactionForThought');
+const deleteReactionForThought = async (req, res) => {
+  try {
+    const { thoughtId, reactionId } = req.params;
+    const data = await Thought.findByIdAndUpdate(
+      { _id: thoughtId },
+      {
+        $pull: { reactions: reactionId },
+      },
+      { returnOriginal: false }
+    );
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(
+      `[ERROR]: Failed to delete reaction for thought | ${error.message}`
+    );
+    return res
+      .status(500)
+      .json({ success: false, error: 'Failed to delete reaction for thought' });
+  }
 };
 
 module.exports = {
