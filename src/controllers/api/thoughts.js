94e3fb92659417eb9409b6dbf6429e8c28a22c56
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-22 22:46:01 +0100

    some thought controllers done + add async

--- src/controllers/api/thoughts.js
+++ src/controllers/api/thoughts.js
@@ -1,11 +1,46 @@
-const getAllThoughts = (req, res) => {
-  res.send('getAllThoughts');
+const getAllThoughts = async (req, res) => {
+  try {
+    const data = await Thought.find({});
+    return res.json({ success: true, data });
+  } catch (err) {
+    console.log(`Failed to get all thoughts || ${err.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: 'Failed to get all thoughts' });
+  }
 };
-const createSingleThought = (req, res) => {
-  res.send('createSingleThought');
+const createSingleThought = async (req, res) => {
+  try {
+    const { username, thoughtText } = req.body;
+    if (username && thoughtText) {
+      const data = await Thought.create({ username, thoughtText });
+      return res.json({ success: true, data });
+    }
+    return res.status(500).json({
+      success: false,
+      error: 'Please fill in all required fields',
+    });
+  } catch (err) {
+    console.log(`Failed to create single thought || ${err.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: 'Failed to create single thought' });
+  }
 };
 const getThoughtById = (req, res) => {
-  res.send('getThoughtById');
+  try {
+    const { thoughtId } = req.params;
+
+    const thought = await Thought.findById(thoughtId);
+
+    return res.json({ success: true, data });
+
+  } catch (err) {
+    console.log(`Failed to get thought by Id || ${err.message}`);
+    return res
+      .status
+      .json({ success: false, error: 'Failed to get thought by Id' });
+  }
 };
 const updateThoughtById = (req, res) => {
   res.send('updateThoughtById');
--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -1,68 +1,69 @@
-const getAllUsers = (req, res) => {
-try {
-  const data = await User.find({}).populate("friends")
-  return res.json({data, success: true})
-} catch (err) {
-  console.log(`Failed to get all users || ${err.message}`);
-  return res.status(500).json({success: false, error: "Failed to get all users"})
-  
-}
+const getAllUsers = async (req, res) => {
+  try {
+    const data = await User.find({}).populate('friends');
+    return res.json({ data, success: true });
+  } catch (err) {
+    console.log(`Failed to get all users || ${err.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: 'Failed to get all users' });
+  }
 };
-const createNewUser = (req, res) => {
-try {
-  const { username, email } = req.body;
- if (username && email) {
-  const data = await User.create({username, email});
-  return res.json({data, success: true });
- } 
- return res.status(404).json({
-  success: false,
-  error: "Please fill in the required username and email fields",
-})
-} catch (err) {
-  console.log(`Failed to create new user || ${error.message}`);
-  return res
-    .status(500)
-    .json({ success: false, error: "Failed to create new user" });
-  
-}
+const createNewUser = async (req, res) => {
+  try {
+    const { username, email } = req.body;
+    if (username && email) {
+      const data = await User.create({ username, email });
+      return res.json({ data, success: true });
+    }
+    return res.status(404).json({
+      success: false,
+      error: 'Please fill in the required username and email fields',
+    });
+  } catch (err) {
+    console.log(`Failed to create new user || ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: 'Failed to create new user' });
+  }
 };
-const getSingleUser = (req, res) => {
-try {
-  const {userId} = req.params;
-  const data = await User.findById(userId);
-  return res.json({data, success: true});
-} catch (err) {
-  console.log(`Failed to get single user || ${err.message}`);
-  return res.status(500).json({success: false, error: "Failed to get single user"})
-}
-
+const getSingleUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const data = await User.findById(userId);
+    return res.json({ data, success: true });
+  } catch (err) {
+    console.log(`Failed to get single user || ${err.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: 'Failed to get single user' });
+  }
 };
-const updateUserById = (req, res) => {
+const updateUserById = async (req, res) => {
   try {
     const { userId } = req.params;
     const data = await User.findByIdAndUpdate(userId, {
-      $set: {...req.body} ,
+      $set: { ...req.body },
     });
-    return res.json({ data, success: true});
+    return res.json({ data, success: true });
   } catch (err) {
     console.log(`Failed to get update user by Id || ${err.message}`);
-    return res.status(500).json({ success: false, error: "Failed to update user by Id" });
+    return res
+      .status(500)
+      .json({ success: false, error: 'Failed to update user by Id' });
   }
 };
-const removeUserById = (req, res) => {
-try {
-  const { userId } = req.params;
-  const data = await User.findByIdAndDelete(userId);
-  return res.json({ success: true, data });
-} catch (err) {
-  console.log(`Failed to delete user by Id || ${error.message}`);
-		return res
-			.status(500)
-			.json({ success: false, error: "Failed to delete user by Id" });
-  
-}
-
+const removeUserById = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const data = await User.findByIdAndDelete(userId);
+    return res.json({ success: true, data });
+  } catch (err) {
+    console.log(`Failed to delete user by Id || ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: 'Failed to delete user by Id' });
+  }
 };
 
 module.exports = {
